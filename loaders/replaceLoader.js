dcd9bdbb773f6a45c3b9db616e2fd7e02c092b9b
zhaoyangkanshijie <zhaoyangkanshijie@users.noreply.github.com>
2021-08-06 08:53:44 +0800

    remove console

--- loaders/replaceLoader.js
+++ loaders/replaceLoader.js
@@ -1,7 +1,7 @@
 //参考：https://juejin.im/post/5cf6777ff265da1bcd37c56f
 
 const loaderUtils = require('loader-utils');
-
+const reg = /(console.log\()(.*)(\))/g;
 //这个函数不能是箭头函数，因为webpack调用loader的时候会对this做一些变更
 module.exports = function(source) {
     //webpack配置文件参数对象
@@ -10,6 +10,7 @@ module.exports = function(source) {
     const options = loaderUtils.getOptions(this);
     const callback = this.async();//调用async才能进行异步操作
     setTimeout(() => {
+        source = source.replace(reg, "")
         const result = source.replace('world', options.name);
         //return source.replace('world', options.name);
         //返回结果
